4bc545b6427d98e78cfae8dab2f357a202c99d43
utrlo <utrlo@student.kit.edu>
2023-11-07 23:44:47 +0100

    Add first test for move in GameBoardHandler

--- ticTacToemproject/src/components/__tests__/HelloWorld.spec.ts
+++ ticTacToemproject/src/components/__tests__/HelloWorld.spec.ts
@@ -1,4 +1,6 @@
-import { describe, it, expect } from 'vitest'
+import { getGameBoard, move } from '../../logic/GameBoardHandler'
+import { GameBoard } from '../../logic/GameBoard'
+import { describe, it, expect, test } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
@@ -7,5 +9,20 @@ describe('HelloWorld', () => {
   it('renders properly', () => {
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
+  })
+})
+
+describe('move', ()=>{
+  test('add piece to board legally', ()=>{
+    move(0, 0, 1)
+    expect(getGameBoard()).toEqual(new GameBoard([
+      [1, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]))
+  })
+
+  test('add piece to board illegally', ()=>{
+    expect(() => move(0, 0, 2)).toThrowError('This piece cannot go there')
   })
 })
