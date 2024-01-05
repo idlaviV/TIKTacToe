8a7feb042f4aba0130c5ab859b35b283e7532907
Leonid Grau <leonid.grau@kit.edu>
2024-01-05 15:13:57 +0100

    converted content from tailwind-config to ES module

--- ticTacToemproject/tailwind.config.js
+++ ticTacToemproject/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
 
