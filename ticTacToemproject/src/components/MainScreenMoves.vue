3c3de88e125f80f829f94136990b6f5e216d8030
Leonid Grau <leonid.grau@kit.edu>
2024-01-14 16:10:14 +0100

    refactored MainScreenMovesPlayButton into own component

--- ticTacToemproject/src/components/MainScreenMoves.vue
+++ ticTacToemproject/src/components/MainScreenMoves.vue
@@ -1,24 +1,10 @@
 <script setup lang="ts">
 import { GameHandler } from '@/logic/GameHandler'
-import { ref } from 'vue';
+import  PlayButton  from './MainScreenMovesPlayButton.vue'
 
 
 const gameHandler = GameHandler.getInstance()
-const autoPlay = ref(true)
 
-const autoPlayOnSymbol = "play_arrow"
-const autoPlayOffSymbol = "pause"
-
-const playPauseButtonSymbol = ref(autoPlayOnSymbol)
-
-const toggleAutoplay = () => {
-    autoPlay.value = !autoPlay.value
-    if (autoPlay.value) {
-        playPauseButtonSymbol.value = autoPlayOnSymbol
-    } else {
-        playPauseButtonSymbol.value = autoPlayOffSymbol
-    }
-}
 
 
 
@@ -31,11 +17,7 @@ const toggleAutoplay = () => {
             <i class="material-icons">
                 skip_next
             </i></v-btn>
-        <v-btn @click="toggleAutoplay">
-            <i  class="material-icons">
-                {{playPauseButtonSymbol}}
-            </i>
-        </v-btn>
+        <PlayButton/>
     </div>
 </template>
 
---
+++ ticTacToemproject/src/components/MainScreenMovesPlayButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const autoPlay = ref(true)
+
+const autoPlayOnSymbol = "play_arrow"
+const autoPlayOffSymbol = "pause"
+const playPauseButtonSymbol = ref(autoPlayOnSymbol)
+
+const toggleAutoplay = () => {
+    autoPlay.value = !autoPlay.value
+    if (autoPlay.value) {
+        playPauseButtonSymbol.value = autoPlayOnSymbol
+    } else {
+        playPauseButtonSymbol.value = autoPlayOffSymbol
+    }
+}
+</script>
+<template>
+        <v-btn @click="toggleAutoplay">
+            <i  class="material-icons">
+                {{ playPauseButtonSymbol }}
+            </i>
+        </v-btn>
+</template>
