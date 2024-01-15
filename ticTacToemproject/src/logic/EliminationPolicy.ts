a0838cfa20b43c77975e5f6984aefb3c70fffab0
Leonid Grau <leonid.grau@kit.edu>
2024-01-15 10:16:25 +0100

    EliminationPolicy checkt alle Verlustzüge

--- ticTacToemproject/src/logic/EliminationPolicy.ts
+++ ticTacToemproject/src/logic/EliminationPolicy.ts
@@ -42,9 +42,9 @@ export class EliminationPolicy implements EvaluationPolicy {
 
     aI.weights.get(lastLooserTurnStart)?.set(lastLooserTurnEnd, 0)
 
-    for (let index = history.length - 3; index > 1; index -= 2) {
+    for (let index = history.length - 3; index > 1; index--) {
       const board = history[index].getNormalForm()
-      for (const value of aI.weights.get(board)?.values() ?? []) {
+      for (const value of aI.weights.get(board)!.values()) {
         if (value !== 0) {
           return
         }
