06e66c923749ebdbfadf834808e6dca89ab531ec
Leonid Grau <leonid.grau@kit.edu>
2024-01-17 09:15:36 +0100

    added tests for PlayerListExpor

---
+++ ticTacToemproject/src/components/__tests__/PlayerListExport.spec.ts
@@ -0,0 +1,30 @@
+import { GameHandler } from "@/logic/GameHandler";
+import { players, updatePlayerList } from "@/utils/PlayerListExport";
+import { beforeEach } from "node:test";
+import { describe, expect, test } from "vitest";
+
+beforeEach(() => {
+    GameHandler.getInstance().destroySingleton()
+})
+
+describe('PlayerListExport', () => {
+    test('Standard case', () => {
+        updatePlayerList()
+        expect(players.value).toEqual([{player:"Human", index:0},{player:"AI", index:1}, {player:"AI2", index:2} ])
+    })
+    test('No AIs', ()=>{
+        GameHandler.getInstance().possiblePlayers = [GameHandler.getInstance().humanPlayer]
+        updatePlayerList()
+        expect(players.value).toEqual([{player:"Human", index:0}])
+    })
+    test('addition of one AI', ()=>{
+        GameHandler.getInstance().destroySingleton()
+        GameHandler.getInstance().createAI(0, "myAI")
+        expect(players.value).toEqual([
+            {player:"Human", index:0},
+            {player:"AI", index:1}, 
+            {player:"AI2", index:2}, 
+            {player:"myAI", index:3}
+        ])
+    })
+})
