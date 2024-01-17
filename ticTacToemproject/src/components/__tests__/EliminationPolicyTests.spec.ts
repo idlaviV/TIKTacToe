6e07c6a4c99a948b4117fc967aa24172b74a7dcf
Leonids Surface <leonid.grau@kit.edu>
2024-01-17 16:51:24 +0100

    format

--- ticTacToemproject/src/components/__tests__/EliminationPolicyTests.spec.ts
+++ ticTacToemproject/src/components/__tests__/EliminationPolicyTests.spec.ts
@@ -26,7 +26,7 @@ describe('getInitialWeight', () => {
 })
 
 describe('applyPolicy with realistic examples', () => {
-  beforeEach(()=>{
+  beforeEach(() => {
     weights.set(
       0,
       new Map([
@@ -86,7 +86,6 @@ describe('applyPolicy with realistic examples', () => {
       ])
     ]
     aI.weights = weights
-    
   })
   test('should not change weights if game is not over', () => {
     handler.winner.value = null
@@ -142,26 +141,93 @@ describe('applyPolicy with realistic examples', () => {
   })
 })
 
-describe('apply Policy with unrealistic examples', ()=>{
-  beforeEach(()=>{
-    history = [new GameBoard(),
-      new GameBoard([[2,0,0],[0,0,0],[0,0,0]]),
-      new GameBoard([[2,2,0],[0,0,0],[0,0,0]]),
-      new GameBoard([[2,2,2],[0,0,0],[0,0,0]]),
-      new GameBoard([[2,2,2],[2,0,0],[0,0,0]]),
-      new GameBoard([[2,2,2],[2,2,0],[0,0,0]]),
-      new GameBoard([[2,2,2],[2,2,2],[0,0,0]]),
+describe('apply Policy with unrealistic examples', () => {
+  beforeEach(() => {
+    history = [
+      new GameBoard(),
+      new GameBoard([
+        [2, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+      ]),
+      new GameBoard([
+        [2, 2, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+      ]),
+      new GameBoard([
+        [2, 2, 2],
+        [0, 0, 0],
+        [0, 0, 0]
+      ]),
+      new GameBoard([
+        [2, 2, 2],
+        [2, 0, 0],
+        [0, 0, 0]
+      ]),
+      new GameBoard([
+        [2, 2, 2],
+        [2, 2, 0],
+        [0, 0, 0]
+      ]),
+      new GameBoard([
+        [2, 2, 2],
+        [2, 2, 2],
+        [0, 0, 0]
+      ])
     ]
-    weights.set(0,new Map([[2,1],[1,1],[10,1]]))
-    weights.set(2,new Map([[22,1],[21,1],[201,1]]))
-    weights.set(22,new Map([[222,1],[221,1],[2201,1]]))
-    weights.set(222,new Map([[2222,1],[2221,1],[22201,1]]))
-    weights.set(2222,new Map([[22222,1],[22221,1],[222201,1]]))
-    weights.set(22222,new Map([[222222,1],[222221,1],[2222201,1]]))
+    weights.set(
+      0,
+      new Map([
+        [2, 1],
+        [1, 1],
+        [10, 1]
+      ])
+    )
+    weights.set(
+      2,
+      new Map([
+        [22, 1],
+        [21, 1],
+        [201, 1]
+      ])
+    )
+    weights.set(
+      22,
+      new Map([
+        [222, 1],
+        [221, 1],
+        [2201, 1]
+      ])
+    )
+    weights.set(
+      222,
+      new Map([
+        [2222, 1],
+        [2221, 1],
+        [22201, 1]
+      ])
+    )
+    weights.set(
+      2222,
+      new Map([
+        [22222, 1],
+        [22221, 1],
+        [222201, 1]
+      ])
+    )
+    weights.set(
+      22222,
+      new Map([
+        [222222, 1],
+        [222221, 1],
+        [2222201, 1]
+      ])
+    )
     aI.weights = weights
     handler.winner.value = 1
   })
-  test("standard loss, expect only one weight change",()=>{
+  test('standard loss, expect only one weight change', () => {
     policy.applyPolicy(aI, history)
     expect(aI.weights.get(2222)?.get(22222)).toEqual(0)
     expect(aI.weights.get(2222)?.get(22221)).toEqual(1)
@@ -174,9 +240,9 @@ describe('apply Policy with unrealistic examples', ()=>{
     expect(aI.weights.get(22)?.get(2201)).toEqual(1)
   })
 
-  test("loss, expect two weights to change",()=>{
-    aI.weights.get(2222)?.set(22221,0)
-    aI.weights.get(2222)?.set(222201,0)
+  test('loss, expect two weights to change', () => {
+    aI.weights.get(2222)?.set(22221, 0)
+    aI.weights.get(2222)?.set(222201, 0)
     policy.applyPolicy(aI, history)
     expect(aI.weights.get(2222)?.get(22222)).toEqual(0)
     expect(aI.weights.get(2222)?.get(22221)).toEqual(0)
@@ -184,27 +250,27 @@ describe('apply Policy with unrealistic examples', ()=>{
     expect(aI.weights.get(222)?.get(2222)).toEqual(1)
     expect(aI.weights.get(222)?.get(2221)).toEqual(1)
     expect(aI.weights.get(222)?.get(22201)).toEqual(1)
-    expect(aI.weights.get(22)?.get(222)).toEqual(0)//<--this changed, too
+    expect(aI.weights.get(22)?.get(222)).toEqual(0) //<--this changed, too
     expect(aI.weights.get(22)?.get(221)).toEqual(1)
     expect(aI.weights.get(22)?.get(2201)).toEqual(1)
   })
 
-  test("loss, expect two weights to change ",()=>{
-    aI.weights.get(222)?.set(2222,0)
-    aI.weights.get(222)?.set(2221,0)
-    aI.weights.get(222)?.set(22201,0)
-      policy.applyPolicy(aI, history)
-      expect(aI.weights.get(2222)?.get(22222)).toEqual(0)
-      expect(aI.weights.get(2222)?.get(22221)).toEqual(1)
-      expect(aI.weights.get(2222)?.get(222201)).toEqual(1)
-      expect(aI.weights.get(222)?.get(2222)).toEqual(0)
-      expect(aI.weights.get(222)?.get(2221)).toEqual(0)
-      expect(aI.weights.get(222)?.get(22201)).toEqual(0)
-      expect(aI.weights.get(22)?.get(222)).toEqual(1)
-      expect(aI.weights.get(22)?.get(221)).toEqual(1)
-      expect(aI.weights.get(22)?.get(2201)).toEqual(1)
-      expect(aI.weights.get(2)?.get(22)).toEqual(0)//<--this changed, too
-      expect(aI.weights.get(2)?.get(21)).toEqual(1)
-      expect(aI.weights.get(2)?.get(201)).toEqual(1)
-    })
+  test('loss, expect two weights to change ', () => {
+    aI.weights.get(222)?.set(2222, 0)
+    aI.weights.get(222)?.set(2221, 0)
+    aI.weights.get(222)?.set(22201, 0)
+    policy.applyPolicy(aI, history)
+    expect(aI.weights.get(2222)?.get(22222)).toEqual(0)
+    expect(aI.weights.get(2222)?.get(22221)).toEqual(1)
+    expect(aI.weights.get(2222)?.get(222201)).toEqual(1)
+    expect(aI.weights.get(222)?.get(2222)).toEqual(0)
+    expect(aI.weights.get(222)?.get(2221)).toEqual(0)
+    expect(aI.weights.get(222)?.get(22201)).toEqual(0)
+    expect(aI.weights.get(22)?.get(222)).toEqual(1)
+    expect(aI.weights.get(22)?.get(221)).toEqual(1)
+    expect(aI.weights.get(22)?.get(2201)).toEqual(1)
+    expect(aI.weights.get(2)?.get(22)).toEqual(0) //<--this changed, too
+    expect(aI.weights.get(2)?.get(21)).toEqual(1)
+    expect(aI.weights.get(2)?.get(201)).toEqual(1)
+  })
 })
