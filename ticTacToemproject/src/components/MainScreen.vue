41561e5a25cecad7803b7e9ba360d0a4a8999039
Leonid Grau <leonid.grau@kit.edu>
2024-01-26 16:45:09 +0100

    added checkboxes for skipping

--- ticTacToemproject/src/components/MainScreen.vue
+++ ticTacToemproject/src/components/MainScreen.vue
@@ -4,10 +4,13 @@ import { drawStatus } from '@/logic/WinnerStatus'
 import MainScreenBoard from './MainScreenBoard.vue'
 import MainScreenMoves from './MainScreenMoves.vue'
 import { player1Name, player2Name } from '@/utils/ActivePlayerExport'
+import { ref } from 'vue'
 
 const gameHandler: GameHandler = GameHandler.getInstance()
 const winner = gameHandler.getWinner()
 const playerOnTurn = gameHandler.getPlayerOnTurn()
+const skipStart = ref(false)
+const skipEvaluation = ref(false)
 
 const startEval = () => {
   gameHandler.performEndOfGameActions()
@@ -29,7 +32,8 @@ const startEval = () => {
             <v-col cols="1">O</v-col><v-col class="text-left" cols="5">{{ player2Name }}</v-col>
           </v-row> </v-col
         ><v-col>
-          <v-btn @click="startEval"> Belohnung anwenden </v-btn>
+            <v-checkbox label="Automatische Belohnung" v-model="skipEvaluation"></v-checkbox>
+            <v-checkbox label="Start überspringen" v-model="skipStart"></v-checkbox>
         </v-col></v-row
       ></v-container
     >
@@ -46,6 +50,7 @@ const startEval = () => {
     <h2 v-show="winner === 1 || winner === 2" class="text-4xl dond-bold mb-8">
       Spieler {{ winner }} hat gewonnen!
     </h2>
+    <v-btn @click="startEval"> Belohnung anwenden </v-btn>
   </div>
 </template>
 <style>
