0fc1ec7fd7d4d723b06c781ed7fd230b35e09d6b
utrlo <utrlo@student.kit.edu>
2024-01-28 11:47:15 +0100

    testfix

--- ticTacToemproject/src/components/__tests__/LabelTest.spec.ts
+++ ticTacToemproject/src/components/__tests__/LabelTest.spec.ts
@@ -2,8 +2,7 @@ import { AIPlayer } from "@/logic/AIPlayer"
 import { EliminationPolicy } from "@/logic/EliminationPolicy"
 import { GameHandler } from "@/logic/GameHandler"
 import { graphExport, initializeHistory } from "@/utils/GraphExport"
-import test from "node:test"
-import { describe, expect } from "vitest"
+import { describe, expect, test } from "vitest"
 
 describe('updateLabel', () => {
   test('first', () => {
@@ -11,6 +10,7 @@ describe('updateLabel', () => {
     const aI = new AIPlayer(new EliminationPolicy(), 'KI-Elimination')
     GameHandler.getInstance().settings.player2 = aI
     initializeHistory()
+    console.log(graphExport.value.edges)
     GameHandler.getInstance().performTurn(0, 0)
     GameHandler.getInstance().performAiTurn()
     console.log(aI.getVertexMap(0))
