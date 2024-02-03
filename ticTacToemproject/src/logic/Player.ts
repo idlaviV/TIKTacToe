258d7efd4924ad34564ef34cd30a410fd98e0c7e
Leonids Surface <leonid.grau@kit.edu>
2024-02-03 13:39:38 +0100

    Playerstats implementiert

--- ticTacToemproject/src/logic/Player.ts
+++ ticTacToemproject/src/logic/Player.ts
@@ -1,3 +1,5 @@
+import { th } from "vuetify/locale"
+
 /**
  * Represents a player.
  * AIPlayers and UserPlayers are possible.
@@ -17,5 +19,36 @@ export abstract class Player {
    * @returns A name for this player. Only used for visualiszation purposes.
    */
   abstract getName(): string
+
+  wins: number = 0
+  losses: number = 0
+  draws: number = 0
+  games: number = 0
+
+  getStats() {
+    return { wins: this.wins, losses: this.losses, draws: this.draws, games: this.games}
+  }
+
+  /**
+   * Register a player game on this player.
+   * @param result Win: 1, Draw: 0, Loss: -1
+   */
+  playedGame(result:number) {
+    this.games++
+    switch (result) {
+      case 0:
+        this.draws++
+        break
+      case 1:
+        this.wins++
+        break
+      case -1:
+        this.losses++
+        break
+      default:
+        throw new Error("Illegal result: " + result)
+    }
+  }
+
 
 }
