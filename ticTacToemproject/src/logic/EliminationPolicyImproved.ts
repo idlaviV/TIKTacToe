24ee0c492aaa5f68a0ef59c3d09702b70da29f06
Leonid Grau <leonid.grau@kit.edu>
2024-02-06 17:49:06 +0100

    more refactoring

--- ticTacToemproject/src/logic/EliminationPolicyImproved.ts
+++ ticTacToemproject/src/logic/EliminationPolicyImproved.ts
@@ -3,8 +3,9 @@ import { EliminationPolicy } from "./EliminationPolicy";
 import type { GameBoard } from "./GameBoard";
 
 export class EliminationPolicyImproved extends EliminationPolicy {
-    applyWinningPolicy(aI: AIPlayer, history: GameBoard[]): void {
+    modifyWeights(ai: AIPlayer, history: GameBoard[], index: number): void {
         throw new Error("Method not implemented.");
     }
+    
 
 }
