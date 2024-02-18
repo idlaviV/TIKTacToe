18ae5619ed4ce1575ede69cb192f576f97f0648c
Joragon1234 <ucflj@student.kit.edu>
2024-02-18 15:01:10 +0100

    Als "?"-Button implementiert

--- ticTacToemproject/src/components/ExplainScreen.vue
+++ ticTacToemproject/src/components/ExplainScreen.vue
@@ -68,59 +68,57 @@ const layouts = {
 }
 </script>
 <template>
-  <v-expansion-panels>
-    <v-expansion-panel>
-      <v-expansion-panel-title class="text-xl">
-        <p class="text-2xl">Was ist das hier?</p>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-row>
-          <v-col cols="12" md="7" lg="8" class="text-lg">
-            <p>
-              Dies ist eine Umsetzung des klassischen Spiels Tic Tac Toe in einer Webanwendung. Du
-              kannst gegen einen Freund oder gegen eine KI spielen.
-            </p>
-            <p>
-              Die KI speichert für jeden möglichen Zug eine Zahl. Je größer die Zahl, desto
-              wahrscheinlicher ist es, dass die KI diesen Zug ausführt. Zum Beispiel wird die KI in
-              der angezeigten Situation den rechten Zug doppelt so wahrscheinlich ausführen als den
-              mittleren Zug. Nach jedem Spiel analysiert die KI die gespielten Züge und passt diese
-              Zahlen an.
-            </p>
-            <p>
-              Es gibt drei verschiedene Arten von KIs zur Auswahl. Jede von ihnen lernt
-              unterschiedlich. Zu Beginn ist die KI sehr schlecht, aber sie wird mit der Zeit
-              lernen, wenn du sie trainierst. Du kannst entweder selbst gegen eine KI spielen oder
-              KIs gegeneinander trainieren lassen.
-            </p>
-          </v-col>
-          <v-col cols="12" md="5" lg="4" align="center">
-            <div>
-              <v-network-graph
-                ref="graph"
-                class="explainGraph"
-                :nodes="nodes"
-                :edges="edges"
-                :layouts="layouts"
-                :configs="configsExplainGraph"
-              >
-                <template #edge-label="{ edgeId, ...slotProps }">
-                  <v-edge-label
-                    :class="`${useDigitalFont === true ? 'dogica text-s' : 'text-xl'}`"
-                    vertical-align="above"
-                    :text="labels[edgeId]"
-                    v-bind="slotProps"
-                  />
-                </template>
-                <template #override-node="{ nodeId }">
-                  <GraphPanelNode :state="nodes[nodeId].boardState" />
-                </template>
-              </v-network-graph></div
-          ></v-col>
-        </v-row>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
+  <v-card class="text-left ma-4 pa-4">
+    <v-card-title class="text-center">Was ist das hier?</v-card-title>
+    <v-divider />
+    <br />
+    <v-row>
+      <v-col cols="12" md="7" lg="8" class="text-lg">
+        <p>
+          Dies ist eine Umsetzung des klassischen Spiels Tic Tac Toe in einer Webanwendung. Du
+          kannst gegen einen Freund oder gegen eine KI spielen.
+        </p>
+        <br />
+        <p>
+          Die KI speichert für jeden möglichen Zug eine Zahl. Je größer die Zahl, desto
+          wahrscheinlicher ist es, dass die KI diesen Zug ausführt. Zum Beispiel wird die KI in
+          der angezeigten Situation den rechten Zug doppelt so wahrscheinlich ausführen als den
+          mittleren Zug. Nach jedem Spiel analysiert die KI die gespielten Züge und passt diese
+          Zahlen an.
+        </p>
+        <br />
+        <p>
+          Es gibt drei verschiedene Arten von KIs zur Auswahl. Jede von ihnen lernt
+          unterschiedlich. Zu Beginn ist die KI sehr schlecht, aber sie wird mit der Zeit
+          lernen, wenn du sie trainierst. Du kannst entweder selbst gegen eine KI spielen oder
+          KIs gegeneinander trainieren lassen.
+        </p>
+      </v-col>
+      <v-col cols="12" md="5" lg="4" align="center">
+        <div>
+          <v-network-graph
+            ref="graph"
+            class="explainGraph"
+            :nodes="nodes"
+            :edges="edges"
+            :layouts="layouts"
+            :configs="configsExplainGraph"
+          >
+            <template #edge-label="{ edgeId, ...slotProps }">
+              <v-edge-label
+                :class="`${useDigitalFont === true ? 'dogica text-s' : 'text-xl'}`"
+                vertical-align="above"
+                :text="labels[edgeId]"
+                v-bind="slotProps"
+              />
+            </template>
+            <template #override-node="{ nodeId }">
+              <GraphPanelNode :state="nodes[nodeId].boardState" />
+            </template>
+          </v-network-graph></div
+      ></v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <style>
--- ticTacToemproject/src/components/StartScreen.vue
+++ ticTacToemproject/src/components/StartScreen.vue
@@ -2,6 +2,7 @@
 import { GameHandler } from '@/logic/GameHandler'
 import { nextGuiState } from '@/logic/GuiState'
 import { players, type PlayersExport, updatePlayerList } from '@/utils/PlayerListExport'
+import ExplainScreen from './ExplainScreen.vue'
 import { ref, type Ref } from 'vue'
 
 /**
@@ -26,12 +27,14 @@ const select1 = ref(0)
  * Model for the player selection for player two
  */
 const select2 = ref(1)
+
+const showHelp = ref(false)
 </script>
 
 <!-- The StartScreen offers the selectiöon of the players for the next game. -->
 <template>
   <v-card class="bg-black" align="center">
-    <h1 class="tictactoe bigarcade">Tic Tac Toe</h1>
+    <h1 class="tictactoe bigarcade inline">Tic Tac Toe</h1>
 
     <v-card class="playerSelection bg-black">
       <div>
@@ -52,13 +55,29 @@ const select2 = ref(1)
         />
       </div>
     </v-card>
-    <v-btn v-on:click="startGame" class="bg-white">Spiel starten</v-btn>
+    <v-card class="playerSelection bg-black">
+      <v-btn v-on:click="startGame" class="bg-white">Spiel starten</v-btn>
+      <v-btn id="help" v-bind:variant="showHelp === true ? 'outlined' : 'tonal'" @click="showHelp = !showHelp">?</v-btn>
+    </v-card>
+    <br />
+    <ExplainScreen v-if="showHelp"/>
   </v-card>
+  <br />
 </template>
 
 <style>
 .playerSelection {
   max-width: 300px;
   justify-self: center;
+  position: relative;
+}
+
+#help {
+  position: absolute;
+  right: 0;
+  padding: 0;
+  min-width: 0;
+  width: 35px;
+  height: 35px;
 }
 </style>
